65eb42c2c1e5a310677f497b5bc62341a3808357
Alan Jantz <kelphyz@gmail.com>
2019-03-16 23:01:52 -0300

    Requisições e eleição

--- Solution/Rules/Ring.cs
+++ Solution/Rules/Ring.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Rules.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Rules
 {
-    public static class Ring
+    public class Ring : IRing
     {
+        private readonly static IList<IProcedure> _activeProcedures = new List<IProcedure>();
+
+        public static IList<IProcedure> ActiveProcedures => _activeProcedures;
     }
 }
